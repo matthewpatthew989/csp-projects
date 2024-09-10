9e6eae4d0ed1f868d42ba26f25a607b0ff8abe3f
matthewpatthew989 <matthew.hansen@ucas-edu.net>
2024-09-10 20:39:33 +0000

    fixed my c program

--- py-c/variableProgram assignment/variableProgram.c
+++ py-c/variableProgram assignment/variableProgram.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void){
-    char name[20];
+    char name[30];
     printf("please tell me your name: \n");
     fgets(name, sizeof(name), stdin);
     printf("hello %s", name);
