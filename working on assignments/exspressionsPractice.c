5afbb1e1ea4df4116f109a1c5160ff7a4ddcecc6
matthewpatthew989 <matthew.hansen@ucas-edu.net>
2024-09-11 20:42:29 +0000

    i made new fulders and started my assighnment

---
+++ working on assignments/exspressionsPractice.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+    int a = 7;
+    int b = 24;
+    int c = 8;
+    int d = 4;
+    int e = 6;
+    int f = 18;
+    int g = 3;
+    int h = 
+    return 0;
+}
