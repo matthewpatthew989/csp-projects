477ad265eae324b0a07416291ca6b13efd59cae1
matthewpatthew989 <matthew.hansen@ucas-edu.net>
2024-09-12 20:42:20 +0000

    started fy financhieal calculator

---
+++ working on assignments/expressions/budget.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void){
+    float income, rent, utilitys, groseries, transportation, expenses, savings, total;
+    float prent, putilitys, pgroseries, pexpenses, ptransportation;
+    printf("this is youer new finacial calculator");
+    printf("how much do you make a month\n");
+    scanf("%f", &income);
+    printf("whats your rent?\n");
+    scanf("%f", &rent);
+    printf("how much are groceries?\n");
+    scanf("%f", &groseries);
+    printf("how much is transportation?\n");
+    scanf("%f", &transportation);
+    printf("what are the expenses of your utilities\n");
+    scanf("%f", &utilitys);
+    expenses = rent + groseries + transportation +utilitys;
+    savings = income
+    printf("here are your expences", expenses);
+    printf("your income is: $%.2f\n");
+    prent = rent/income;
+    putilitys = utilitys/income;
+    pgroseries = groseries/income;
+    ptransportation = transportation/income;
+
+    return 0;
+}
