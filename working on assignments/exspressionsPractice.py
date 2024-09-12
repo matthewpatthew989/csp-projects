c3296af82e865a73aa433414b7e3da03e82a1b3d
matthewpatthew989 <matthew.hansen@ucas-edu.net>
2024-09-12 02:51:39 +0000

    finished my python project

---
+++ working on assignments/exspressionsPractice.py
@@ -0,0 +1,25 @@
+a = 7
+b = 24
+c = 8
+d = 4
+e = 6
+f = 18
+g = 3
+h = 2
+i = 5
+j = 12
+k = 72
+l = 9
+m = 17
+n = 1
+o = 15
+p = 22
+print(a-b/c*d+e)
+print(f/g-a+h*5)
+print(e*d/j+k/c-l)
+print((m-e/h)+d*g)
+print(-h*(n*d-h/h)+(e+h-g))
+print(-n*((g-d*a)/i)-h*b/e-h*b/e)
+print((g*i**h/o)-(i-h**h))
+print((n**d*h**h+g**g)-h**i/d)
+print((p/h-h*i)**h+(d-e/e)**h)
