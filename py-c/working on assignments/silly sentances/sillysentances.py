257b8e8409194abd2e4562ed598f7a6a7f360de8
matthewpatthew989 <matthew.hansen@ucas-edu.net>
2024-09-20 00:04:15 +0000

    finished my assinghment

---
+++ py-c/working on assignments/silly sentances/sillysentances.py
@@ -0,0 +1,6 @@
+name = input("type a name: ")
+place = input("type a place: ")
+verb = input("type a verb: ")
+noun =  input("type a noun: ")
+sentance = name+" went to the "+place+" where he "+verb+" and bought a "+noun+"."
+print(sentance)
